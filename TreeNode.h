3d4246b9872dc9cb538a3d0ab38614ce42857965
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-06-07 15:58:08 +0300

    Make all tree node fields public
    Just to don't write all that boilerplate code with getters & setters.

--- TreeNode.h
+++ TreeNode.h
@@ -7,13 +7,13 @@
 
 class TreeNode {
 public:
-    friend class Visitor;
     virtual double accept(const Visitor &visitor) = 0;
 };
 
 class StatementListNode : public TreeNode {
-    std::vector<TreeNode> statements;
 public:
+    std::vector<TreeNode> statements;
+
     double accept(const Visitor &visitor) override { return visitor.visit(this); };
 };
 
@@ -21,7 +21,6 @@ class ExpressionNode : public TreeNode {
 };
 
 class OperatorNode : public ExpressionNode {
-
 public:
     ExpressionNode *left;
     ExpressionNode *right;
@@ -33,33 +32,42 @@ public:
 };
 
 class VariableNode : public ExpressionNode {
+public:
     const std::string name;
 
-public:
     explicit VariableNode(std::string name) : name(std::move(name)) {};
 
     double accept(const Visitor &visitor) override { return visitor.visit(this); };
 };
 
 class ConstantNode : public ExpressionNode {
+public:
     const Number *value;
 
-public:
     explicit ConstantNode(const Number *value) : value(value) {};
+
     double accept(const Visitor &visitor) override { return visitor.visit(this); };
 };
 
 class AssignmentNode : public TreeNode {
+public:
     VariableNode *variable;
     ExpressionNode *expression;
-public:
+
+    AssignmentNode(VariableNode *variable, ExpressionNode *expression) : variable(variable), expression(expression) {}
+
     double accept(const Visitor &visitor) override { return visitor.visit(this); };
 };
 
 class BranchNode : public TreeNode {
+public:
     ExpressionNode *condition;
     StatementListNode *ifTrue;
     StatementListNode *ifFalse;
-public:
+
+    BranchNode(ExpressionNode *condition, StatementListNode *ifTrue, StatementListNode *ifFalse) : condition(condition),
+                                                                                                   ifTrue(ifTrue),
+                                                                                                   ifFalse(ifFalse) {}
+
     double accept(const Visitor &visitor) override { return visitor.visit(this); };
 };
