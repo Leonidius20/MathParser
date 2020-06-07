6c04c6510480590e97c9765428ea63c40690b69c
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-06-07 18:22:36 +0300

    Implement operator optimizations

--- visitor.cpp
+++ visitor.cpp
@@ -76,25 +76,90 @@ double OptimizationVisitor::visit(VariableNode *node) {
     return 0;
 }
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "hicpp-multiway-paths-covered" // Ignore "not all switch paths considered (add default)"
+
 double OptimizationVisitor::visit(OperatorNode *node) {
     TreeNode *parent = stack.peek();
+
+    // Try to optimize children first.
     stack.push(node);
-
-    // TODO:
-    //   - 0 [*/] expression -> 0
-    //   - 1 * expression -> expression
-    //   - expression ^ 1 -> expression
-    //   - expression ^ 0 -> 1
-    //   - 1 ^ expression -> 1
-    //   - 0 ^ expression -> 0
-    //   - expression [*] 0 -> 0
-    //   - 0 [+-] expression -> expression
-    //   - expression [+-] 0 -> expression
-    //   - expression [*/] 1 -> expression
-
+    node->left->accept(this);
+    node->right->accept(this);
     stack.pop();
+
+    char signature = node->oper->getSignature();
+    auto constantZero = new ConstantNode(new Number(0));
+    auto constantOne = new ConstantNode(new Number(1));
+
+    // Try optimize right first because `0^0 = 1`
+    if (node->right->getType() == constant) {
+        auto value = dynamic_cast<ConstantNode *>(node->right)->value->getValue();
+
+        if (value == 0) {
+            switch (signature) {
+                // expression [*] 0 -> 0
+                case '*':
+                    parent->replaceChild(node, constantZero);
+                    break;
+
+                    // expression [+-] 0 -> expression
+                case '+':
+                case '-':
+                    parent->replaceChild(node, node->left);
+                    break;
+
+                    // expression ^ 0 -> 1
+                case '^':
+                    parent->replaceChild(node, constantOne);
+                    break;
+            }
+        } else if (value == 1) {
+            switch (signature) {
+                // expression [*/^] 1 -> expression
+                case '*':
+                case '/':
+                case '^':
+                    parent->replaceChild(node, node->left);
+                    break;
+            }
+        }
+    } else if (node->left->getType() == constant) {
+        auto value = dynamic_cast<ConstantNode *>(node->left)->value->getValue();
+
+        if (value == 0) {
+            switch (signature) {
+                // 0 [*/^] expression -> 0
+                case '*':
+                case '/':
+                case '^':
+                    parent->replaceChild(node, constantZero);
+                    break;
+
+                    // 0 [+-] expression -> expression
+                case '+':
+                case '-':
+                    parent->replaceChild(node, node->right);
+                    break;
+            }
+        } else if (value == 1)
+            switch (signature) {
+                // 1 * expression -> expression
+                case '*':
+                    parent->replaceChild(node, node->right);
+                    break;
+
+                    // 1 ^ expression -> 1
+                case '^':
+                    parent->replaceChild(node, constantOne);
+                    break;
+            }
+    }
+
     return 0;
 }
+
+#pragma clang diagnostic pop
 
 double OptimizationVisitor::visit(AssignmentNode *node) {
     stack.push(node);
